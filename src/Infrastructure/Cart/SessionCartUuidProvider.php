941a4de1c01b1b7c1948b96c2b63885a25a94f58
asymptota <konrad.zuk1@gmail.com>
2024-07-23 14:22:19 +0200

    Cart Uuid fixed value

--- src/Infrastructure/Cart/SessionCartUuidProvider.php
+++ src/Infrastructure/Cart/SessionCartUuidProvider.php
@@ -15,6 +15,6 @@ class SessionCartUuidProvider implements CartUuidProvider
         // TODO: providing cart uuid from session or set new one in session
         // TODO: log if cart uuid not found in session for debug purpose
 
-        return Uuid::uuid4();
+        return Uuid::fromString('1a351d13-c4c8-4e13-8441-f89901c76276');
     }
 }
